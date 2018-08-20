cc78685515043070a803b3d4850ef1ae3f26d10a
Dani Larsen <danilarsen16@yahoo.com>
2018-08-20 10:00:58 -0500

    rename

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,18 @@ import React from "react";
 import "./Header.css";
 
 const Header = props => (
-  <div className="header">
+
+
+  <div className="jumbotron jumbotron-fluid">
+  <div className="container">
+    <h1 className="display-4">Click an image to begin!</h1>
     <div className="title">{props.children}</div>
     <div className="scores">
       Score: {props.score} Highscore: {props.highscore}
     </div>
   </div>
+</div>
+
 );
 
 export default Header;
